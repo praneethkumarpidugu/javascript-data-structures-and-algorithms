ddba98e0a7b6c5bb170cb0a1486a329a474bbe10
praneethkumarpidugu <pidugu2007@gmail.com>
2019-02-04 00:38:58 -0500

    Udemy Sweet Solution to chunked

--- AlgoCasts-master/exercises/chunk/index.js
+++ AlgoCasts-master/exercises/chunk/index.js
@@ -8,37 +8,62 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-    let i = 0;
-    let j = 0;
-    let myResult = [];
-    let smallerArray = [];
-    while (i < array.length)
-    {
-        if(array.length == 1)
-        {
-            myResult.push([array[i]]);
-            break;
-        }
-        while (j < size)
-        {
-            if(array[i] == undefined)
-            {
-                break;
-            } 
-            smallerArray.push(array[i]);
-            array.shift();
-            j++;
-        }
-        j=0;
-        i=0;
-        myResult.push(smallerArray);
-        smallerArray = [];   
-    }
-    console.log(myResult);
-    return myResult;
+// function chunk(array, size) {
+//     let i = 0;
+//     let j = 0;
+//     let myResult = [];
+//     let smallerArray = [];
+//     while (i < array.length)
+//     {
+//         if(array.length == 1)
+//         {
+//             myResult.push([array[i]]);
+//             break;
+//         }
+//         while (j < size)
+//         {
+//             if(array[i] == undefined)
+//             {
+//                 break;
+//             } 
+//             smallerArray.push(array[i]);
+//             array.shift();
+//             j++;
+//         }
+//         j=0;
+//         i=0;
+//         myResult.push(smallerArray);
+//         smallerArray = [];   
+//     }
+//     console.log(myResult);
+//     return myResult;
     
+// }
+
+// Udemy Solution # 1
+// Create an empty array to hold chunks called chunked
+// For each element in the chunked array
+//         Retrieve the last element in the chunked array
+//         If last element doesnot exist or if its length is equal to chunk size
+//              push a new chunk into chunked with current element
+//         else add the current element into the chunked.
+function chunk(array, size)
+{
+    const chunked = [];
+    for(let element of array)
+    {
+        const last = chunked[chunked.length -1];
+        if(!last || last.length === size)
+        {
+            chunked.push([element]);
+        }
+        else {
+            last.push(element);
+        }
+    }
+    return chunked;
 }
 
-chunk([1,2,3],1);
+chunk([1, 2, 3], 2);
+
 module.exports = chunk;
