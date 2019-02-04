2ed4feed3d8f2567e3a714b0306838d0e63fd88e
praneethkumarpidugu <pidugu2007@gmail.com>
2019-02-04 01:06:13 -0500

    Udemy Sweet Solution to chunked

--- AlgoCasts-master/exercises/chunk/index.js
+++ AlgoCasts-master/exercises/chunk/index.js
@@ -47,23 +47,59 @@
 //         If last element doesnot exist or if its length is equal to chunk size
 //              push a new chunk into chunked with current element
 //         else add the current element into the chunked.
+// function chunk(array, size)
+// {
+//     const chunked = [];
+//     for(let element of array)
+//     {
+//         const last = chunked[chunked.length -1];
+//         if(!last || last.length === size)
+//         {
+//             chunked.push([element]);
+//         }
+//         else {
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+// }
+
+// Usage of slice. my solution
+// function chunk(array, size)
+// {
+//     const chunked = [];
+//     let index = 0;
+
+//     while (index < array.length)
+//     {
+//         let smallerChunk = array.slice(index,size);
+//         if(smallerChunk.length === size)
+//         {
+//             chunked.push(smallerChunk);
+//         }
+//         else {
+//             chunked.push(array.slice(index));
+//         }
+//         index = index + size;
+//     }
+    
+    
+// }
+
+// Usage of Slice Udemy Solution
 function chunk(array, size)
 {
     const chunked = [];
-    for(let element of array)
+    let index = 0;
+    while(index < array.length)
     {
-        const last = chunked[chunked.length -1];
-        if(!last || last.length === size)
-        {
-            chunked.push([element]);
-        }
-        else {
-            last.push(element);
-        }
+        chunked.push(array.slice(index,index + size));
+        index = index + size;
     }
+    
     return chunked;
 }
 
-chunk([1, 2, 3], 2);
+chunk([1, 2, 3,4], 2);
 
 module.exports = chunk;
