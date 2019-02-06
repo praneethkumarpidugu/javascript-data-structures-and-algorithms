fab919a62525e1681b14d3482cf280466e3d65b3
praneethkumarpidugu <pidugu2007@gmail.com>
2019-02-06 00:32:40 -0500

    My solution to Capitilize

--- AlgoCasts-master/exercises/capitalize/index.js
+++ AlgoCasts-master/exercises/capitalize/index.js
@@ -7,6 +7,35 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const splitWord =  str.split(' ');
+
+    let i=0;
+    let k=0;
+    let constFinalWord = [];
+    let joinWord = [];
+    while(i < splitWord.length)
+    {
+        let char = splitWord[i].split('');
+        while (k < char.length)
+        {
+            if(k === 0)
+            {
+                joinWord.push(char[k].toUpperCase());
+                k++;
+                continue;
+            }
+            joinWord.push(char[k]);
+            k++;
+        }
+        k=0;
+        constFinalWord.push(joinWord.join(''));
+        joinWord = [];
+        i++;
+    }
+    return constFinalWord.join(' ');
+}
+
+capitalize('a short sentence');
 
 module.exports = capitalize;
