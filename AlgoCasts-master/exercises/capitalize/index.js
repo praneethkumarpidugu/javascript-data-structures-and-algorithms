abc12693eb8f4fb8ad3a90932fd2426783165456
praneethkumarpidugu <pidugu2007@gmail.com>
2019-02-07 23:21:54 -0500

    capitalize solution

--- AlgoCasts-master/exercises/capitalize/index.js
+++ AlgoCasts-master/exercises/capitalize/index.js
@@ -7,35 +7,60 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+// function capitalize(str) {
+//     const splitWord =  str.split(' ');
+
+//     let i=0;
+//     let k=0;
+//     let constFinalWord = [];
+//     let joinWord = [];
+//     while(i < splitWord.length)
+//     {
+//         let char = splitWord[i].split('');
+//         while (k < char.length)
+//         {
+//             if(k === 0)
+//             {
+//                 joinWord.push(char[k].toUpperCase());
+//                 k++;
+//                 continue;
+//             }
+//             joinWord.push(char[k]);
+//             k++;
+//         }
+//         k=0;
+//         constFinalWord.push(joinWord.join(''));
+//         joinWord = [];
+//         i++;
+//     }
+//     return constFinalWord.join(' ');
+// }
+
+// function capitalize(str) {
+//     let initialString = [];
+//     let i=0,j=1;
+//     for(let char of str.split(' '))
+//     {
+//         initialString.push(char[i].toUpperCase()+char.slice(j));
+//     }
+
+//     return initialString.join(' ');
+    
+// }
 function capitalize(str) {
-    const splitWord =  str.split(' ');
-
-    let i=0;
-    let k=0;
-    let constFinalWord = [];
-    let joinWord = [];
-    while(i < splitWord.length)
+    let result = str[0].toUpperCase();
+    for(let i=1; i<str.length;i++)
     {
-        let char = splitWord[i].split('');
-        while (k < char.length)
+        if(str[i -1] === ' ')
         {
-            if(k === 0)
-            {
-                joinWord.push(char[k].toUpperCase());
-                k++;
-                continue;
-            }
-            joinWord.push(char[k]);
-            k++;
+            result = result + str[i].toUpperCase()
+        } else {
+            result = result + str[i];
         }
-        k=0;
-        constFinalWord.push(joinWord.join(''));
-        joinWord = [];
-        i++;
     }
-    return constFinalWord.join(' ');
+    return result
+    
 }
-
 capitalize('a short sentence');
 
 module.exports = capitalize;
