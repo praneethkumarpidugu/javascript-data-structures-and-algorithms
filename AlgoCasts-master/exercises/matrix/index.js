9175fc7043d75e60795bd6640765ee02fdc9369a
praneethkumarpidugu <pidugu2007@gmail.com>
2019-02-12 16:16:22 -0500

    spiral matrix representation && vowels solved

--- AlgoCasts-master/exercises/matrix/index.js
+++ AlgoCasts-master/exercises/matrix/index.js
@@ -14,7 +14,75 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+// matrix (5)
+// [[1, 2, 3, 4, 5],
+// [16,17,18,19,6],
+// [15,24,25,20,7],
+// [14,23,22,21,8],
+// [13,12,11,10,9]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+
+    for (let i=0; i<n; i++)
+    {
+        results.push([])
+    }
+    
+    let counter = 1;
+
+    let start_row = 0;
+    let end_row = n - 1;
+    let start_col = 0;
+    let end_col = n - 1;
+
+    while (start_row <= end_row && start_col <= end_col)
+    {
+        // first column
+        for(let i=start_col; i<=end_col; i++)
+        {
+            results[start_row][i] = counter;
+            counter++;
+        }
+        start_row++;
+
+        // right column
+        for(let i=start_row; i<=end_row; i++)
+        {
+            results[i][end_col] = counter;
+            counter++;
+        }
+        end_col--;
+
+        // bottow row
+        for(let i=end_col; i>= start_col; i--)
+        {
+            results[end_row][i] = counter;
+            counter++;
+        }
+        end_row--;
+
+        // left column
+
+        for(let i=end_row; i>=start_row; i--)
+        {
+            results[i][start_col] = counter;
+            counter++;
+        }
+        start_col++;
+
+    }
+    
+    let j = 0;
+    while(j < results.length)
+    {
+        console.log(`col:${j} =>  ${results[j]}`);
+        j++
+        
+    }
+    
+
+}
+matrix(3);
 
 module.exports = matrix;
--- AlgoCasts-master/exercises/vowels/index.js
+++ AlgoCasts-master/exercises/vowels/index.js
@@ -7,34 +7,51 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {
-    let charMap1 = charMap(str);
-    let counter = 0;
-    let vowels = 'aeiou'
-    for(let char1 of vowels)
-    {
-        if (char1 in charMap1)
-        {
-            counter += 1;
-        }
-    }
-    return counter;
-}
-function charMap(text)
-{
-    let result_map = {};
-    for (let char of text.toLowerCase())
-    {
-        if (char in result_map)
-        {
-            result_map[char] += 1;
-        }
-        else
-        {
-            result_map[char] = 1
-        }
-    }
-    return result_map;
-}
+// function vowels(str) {
+//     let charMap1 = charMap(str);
+//     let counter = 0;
+//     let vowels = 'aeiou'
+//     for(let char1 of vowels)
+//     {
+//         if (char1 in charMap1)
+//         {
+//             counter += 1;
+//         }
+//     }
+//     return counter;
+// }
+// function charMap(text)
+// {
+//     let result_map = {};
+//     for (let char of text.toLowerCase())
+//     {
+//         if (char in result_map)
+//         {
+//             result_map[char] += 1;
+//         }
+//         else
+//         {
+//             result_map[char] = 1
+//         }
+//     }
+//     return result_map;
+// }
 
+// function vowels(str) {
+// //  let counter = 0;
+// //  let checker = 'aeiou'.split('')
+// //  for(let char of str.toLowerCase())
+// //  {
+// //      if (checker.includes(char)){
+// //         counter +=1
+// //      }
+// //  }
+// //  return counter;   
+// }
+
+function vowels(str)
+{
+    let checker = str.match(/[aeiou]/gi)
+    return checker ? checker.length : 0
+}
 module.exports = vowels;
